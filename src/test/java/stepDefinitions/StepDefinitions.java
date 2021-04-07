2dd7e105432cee98785e3653f0f90dc6603eb84d
AnnaMakuch <a.makuchova@gmail.com>
2021-04-07 16:06:37 +0100

    adding method sendKeys and username randomizer

--- src/test/java/stepDefinitions/StepDefinitions.java
+++ src/test/java/stepDefinitions/StepDefinitions.java
@@ -16,6 +16,13 @@ public class StepDefinitions {
 
     private WebDriver driver;
 
+    // Method to enter value into text field:
+    public void sendKeys(By by, String input) {
+        new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(by));
+        WebElement textField = driver.findElement(by);
+        textField.sendKeys(input);
+    }
+
     @Given("Open web browser and go to Mailchimp")
     public void open_web_browser_and_go_to_mailchimp() {
 
@@ -30,25 +37,31 @@ public class StepDefinitions {
         WebElement cookies = driver.findElement(By.id("onetrust-accept-btn-handler"));
         WebDriverWait wait = new WebDriverWait(driver, 10);
         wait.until(ExpectedConditions.elementToBeClickable(cookies));
-        cookies.click();
+        if (cookies.isDisplayed()) {
+            cookies.click();
+        }
     }
 
     @Given("I enter an email address as {string}")
-    public void i_enter_an_email_address_as(String string1) {
-        WebElement emailTextField = driver.findElement(By.id("email"));
-        emailTextField.sendKeys(string1);
+    public void i_enter_an_email_address_as(String inputEmail) {
+        sendKeys(By.id("email"), inputEmail);
     }
 
     @Given("I enter a new username as {string}")
-    public void i_enter_a_new_username_as(String string2) {
-        WebElement userName = driver.findElement(By.id("new_username"));
-        userName.sendKeys(string2);
+    public void i_enter_a_new_username_as(String inputUsername) {
+        if (inputUsername.equals("carrot")) {
+            int randomNumber = (int) (Math.random() * 100000000);
+            String randomUserName = inputUsername + (String.valueOf(randomNumber));
+            sendKeys(By.id("new_username"), randomUserName);
+        } else if (inputUsername.equals("longUserName")) {
+            sendKeys(By.id("new_username"), "strawberry13571strawberry13571strawberry13571strawberry13571strawberry13571strawberry13571strawberry13571");
+        } else
+            sendKeys(By.id("new_username"), inputUsername);
     }
 
     @Given("I also enter a new password as {string}")
-    public void i_also_enter_a_new_password_as(String string3) {
-        WebElement password = driver.findElement(By.id("new_password"));
-        password.sendKeys(string3);
+    public void i_also_enter_a_new_password_as(String inputPassword) {
+        sendKeys(By.id("new_password"), inputPassword);
     }
 
     @When("I press the sign up button")
@@ -58,41 +71,38 @@ public class StepDefinitions {
         WebDriverWait wait = new WebDriverWait(driver, 10);
         wait.until(ExpectedConditions.elementToBeClickable(signUpButton));
 
-        signUpButton.click();
+        if (signUpButton.isDisplayed()) {
+            signUpButton.click();
+        }
     }
 
-    @Then("I am registered as a new user")
-    public void i_am_registered_as_a_new_user() {
+    @Then("Registration is successful\\/unsuccessful, as displayed with {string}")
+    public void registration_is_successful_unsuccessful_as_displayed_with(String status) {
         String bodyText = driver.findElement(By.tagName("body")).getText();
 
-        if (bodyText.contains("with a link to activate your account")) {
+        if (status.equals("success")) {
             boolean textActivateAccount = bodyText.contains("with a link to activate your account");
             assertTrue(textActivateAccount);
+        } else if (status.equals("fail")) {
+
+            if (bodyText.contains("Enter a value less than 100 characters long")) {
+                boolean text100Characters = bodyText.contains("Enter a value less than 100 characters long");
+                assertTrue(text100Characters);
+
+            } else if (bodyText.contains("evil twin")) {
+                boolean textEvilTwin = bodyText.contains("evil twin");
+                assertTrue(textEvilTwin);
+
+            } else if (bodyText.contains("Please enter a value")) {
+                boolean textEmailMissing = bodyText.contains("Please enter a value");
+                assertTrue(textEmailMissing);
+
+            } else {
+                System.out.println("Something went wrong.");
+            }
         }
 
-        else if (bodyText.contains("Enter a value less than 100 characters long")) {
-            boolean text100Characters = bodyText.contains("Enter a value less than 100 characters long");
-            assertTrue(text100Characters);
-        }
-
-        else if (bodyText.contains("evil twin")){
-            boolean textEvilTwin = bodyText.contains("evil twin");
-            assertTrue(textEvilTwin);
-        }
-
-        else if (bodyText.contains("Please enter a value")){
-            boolean textEmailMissing = bodyText.contains("Please enter a value");
-            assertTrue(textEmailMissing);
-        }
-
-        else{
-            System.out.println("Something went wrong.");
-        }
-
-
-        //Assert.assertTrue("successfully signed up", bodyText.contains("with a link to activate your account"));
-        //Assert.assertTrue("username too long", bodyText.contains("Enter a value less than 100 characters long"));
-
-        //driver.quit();
     }
+
+
 }
--- src/test/resources/features/mailchimp.feature
+++ src/test/resources/features/mailchimp.feature
@@ -6,15 +6,15 @@ Feature: Sign up a new user to Mailchimp
   @signUp
   Scenario Outline: Sign up to Mailchimp
     Given I accept cookies
-    Given I enter an email address as "<email>"
+    And I enter an email address as "<email>"
     And I enter a new username as "<username>"
     And I also enter a new password as "<password>"
     When I press the sign up button
-    Then I am registered as a new user
+    Then Registration is successful/unsuccessful, as displayed with "<status>"
     Examples:
-      | email                     | username                                                                                                  | password   |
-      | carrot13575@gmail.com     | carrot13575                                                                                               | Asdf123#   |
-      | strawberry13571@gmail.com | strawberry13571strawberry13571strawberry13571strawberry13571strawberry13571strawberry13571strawberry13571 | Asdf123# # |
-      | potato13579@gmail.com     | potato                                                                                                    | Asdf123# # |
-      |                           | lemon13579                                                                                                | Asdf123# # |
+      | email                        | username      | password | status  |
+      | carrot38403868@gmail.com     | carrot        | Asdf123# | success |
+      | strawberry38403868@gmail.com | longUserName  | Asdf123# | fail    |
+      | potato38403868@gmail.com     | potato        | Asdf123# | fail    |
+      |                              | lemon38403868 | Asdf123# | fail    |
 
